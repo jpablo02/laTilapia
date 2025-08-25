1b1cd96a87ff256ac38fc1a80d377f1620405de9
juan <jpredcoin@gmail.com>
2025-08-25 16:01:12 -0500

    terminando el demo

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class", // 👈 controla el tema solo si pones la clase "dark"
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
